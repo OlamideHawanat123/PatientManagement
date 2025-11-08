fc81671acd150c1a448850243164c5be099320a6
OlamideHawanat123 <raheemhawanat@gmail.com>
2025-11-08 03:43:54 +0100

    first commit

---
+++ patient-service/src/main/java/com/patientservice/model/Patient.java
@@ -0,0 +1,31 @@
+package com.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+}
