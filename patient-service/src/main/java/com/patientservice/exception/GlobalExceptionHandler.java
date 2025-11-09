e529e55fb52e902b6053186fe3d72397141394d3
OlamideHawanat123 <raheemhawanat@gmail.com>
2025-11-09 16:47:15 +0100

    update patient method started

--- patient-service/src/main/java/com/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/patientservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.patientservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -15,6 +17,16 @@ public class GlobalExceptionHandler {
         Map<String, String> errors = new HashMap<>();
         e.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
+    }
 
+    public ResponseEntity<Map<String, String>> handleEmailExistException(EmailAlreadyExistsException e){
+        log.warn("Email already exist {}", e.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Email already exist");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    public ResponseEntity<Map<String, String>> handlePatientException(PatientNotFoundException e){
+        Map<String, String> errors = new HashMap<>();
     }
 }
--- patient-service/src/main/java/com/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/patientservice/service/PatientService.java
@@ -2,12 +2,14 @@ package com.patientservice.service;
 
 import com.patientservice.dto.PatientRequestDto;
 import com.patientservice.dto.PatientResponseDto;
+import com.patientservice.exception.EmailAlreadyExistsException;
 import com.patientservice.mapper.PatientMapper;
 import com.patientservice.model.Patient;
 import com.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -22,9 +24,13 @@ public class PatientService {
     }
 
     public PatientResponseDto createPatient(PatientRequestDto patientRequestDto){
-        if (patientRepository.existsByEmail(patientRequestDto.getEmail())) throw new EmailAlreadyExistException("A patient with this email" + "already exist", patientRequestDto.getEmail());
+        if (patientRepository.existsByEmail(patientRequestDto.getEmail())) throw new EmailAlreadyExistsException("A patient with this email" + "already exist" + patientRequestDto.getEmail());
         Patient patient = patientRepository.save(PatientMapper.mapToModel(patientRequestDto));
         return PatientMapper.mapToDto(patient);
+    }
+
+    public PatientResponseDto updatePatient(UUID id, PatientRequestDto patientRequestDto){
+//        Patient patient = patientRepository.findById(id).orElse(()->{ throw new PatientNotFoundException})
     }
 }
 
